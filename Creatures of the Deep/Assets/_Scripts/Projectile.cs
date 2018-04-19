3350215d808c597bed562718b2cbbacab48ed9b4
NooraD <ndaoud@myjmc.edu.au>
2018-04-19 16:33:24 +1000

    19/04/2018 4:30pm

---
+++ Creatures of the Deep/Assets/_Scripts/Projectile.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public AudioSource[] sounds = new AudioSource[5];
+    public Transform plane;
+    public float yAxis;
+
+    void Start()
+    {
+        GameObject enemyExplosionSound = GameObject.Find("Enemy_Explosion");
+        sounds[0] = enemyExplosionSound.GetComponent<AudioSource>();
+
+        GameObject enemyHitSound = GameObject.Find("Enemy_Hit");
+        sounds[1] = enemyHitSound.GetComponent<AudioSource>();
+
+        plane = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        yAxis = plane.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x >= 15)
+        {
+            Destroy(gameObject);
+        }
+
+        if (transform.position.x <= -15)
+        {
+            Destroy(gameObject);
+        }
+
+        transform.rotation = Quaternion.FromToRotation(plane.position, plane.position);
+        //transform.rotation = Quaternion.LookRotation(gameObject.GetComponent<Rigidbody>().velocity);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+         if (other.tag == "Enemy")
+        {
+            sounds[0].Play();
+        }
+
+        if (other.tag == "Enemy_2")
+        {
+            sounds[0].Play();
+        }
+
+        if (other.tag == "Enemy_3")
+        {
+            sounds[0].Play();
+        }
+
+        if (other.tag == "Enemy_4")
+        {
+            sounds[0].Play();
+        }
+
+        if (other.tag == "Boss")
+        {
+            sounds[1].Play();
+        }
+
+        if(other.tag == "Switch")
+        {
+            sounds[1].Play();
+            Switches.hp--;
+            Destroy(gameObject);
+            Debug.Log("Ouch");
+            
+        }
+    }
+}
+
+
+
+
+
---
+++ Creatures of the Deep/Assets/_Scripts/Switches.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Switches : MonoBehaviour
+{
+    public static int hp;
+    public GameObject explosion;
+
+    // Use this for initialization
+    void Start()
+    {
+        hp = 7;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(hp.ToString());
+        /*if (health == 0)
+        {
+            Destroyed();
+        }
+        if(Plane.buffCounts[0] == 0)
+        {
+            health = 5;
+        }
+        else if (Plane.buffCounts[0] == 1)
+        {
+            health = 
+        }*/
+
+        if (hp == 0)
+        {
+            Destroyed();
+        }
+    }
+
+    void Destroyed()
+    {
+        Destroy(gameObject);
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        //Animate Door
+    }      
+}
